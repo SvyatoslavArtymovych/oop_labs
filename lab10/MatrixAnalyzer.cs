4d13558b51a7efbba089ec4445283debbfd65d45
SvyatoslavArtymovych <svyatoslav.artymovych@gmail.com>
2025-05-01 20:08:28 +0300

    lab10

---
+++ lab10/MatrixAnalyzer.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+
+class MatrixAnalyzer
+{
+    private readonly int[,] matrix;
+    private readonly int rows, cols;
+
+    public MatrixAnalyzer(int[,] matrix)
+    {
+        this.matrix = matrix;
+        rows = matrix.GetLength(0);
+        cols = matrix.GetLength(1);
+    }
+
+    public int CountRowsWithoutZeros()
+    {
+        int count = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            bool hasZero = false;
+            for (int j = 0; j < cols; j++)
+            {
+                if (matrix[i, j] == 0)
+                {
+                    hasZero = true;
+                    break;
+                }
+            }
+            if (!hasZero) count++;
+        }
+        return count;
+    }
+
+    public int? MaxDuplicateNumber()
+    {
+        Dictionary<int, int> freq = new Dictionary<int, int>();
+        foreach (int val in matrix)
+        {
+            if (freq.ContainsKey(val))
+                freq[val]++;
+            else
+                freq[val] = 1;
+        }
+
+        int? max = null;
+        foreach (var pair in freq)
+        {
+            if (pair.Value > 1)
+            {
+                if (max == null || pair.Key > max)
+                    max = pair.Key;
+            }
+        }
+
+        return max;
+    }
+}
---
+++ lab10/Program.cs
@@ -0,0 +1,24 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        int[,] data = {
+            {1, 2, 3},
+            {4, 0, 6},
+            {7, 8, 9},
+            {1, 2, 3}
+        };
+
+        MatrixAnalyzer analyzer = new MatrixAnalyzer(data);
+
+        int rowsWithoutZeros = analyzer.CountRowsWithoutZeros();
+        Console.WriteLine("Number of rows without zeros: " + rowsWithoutZeros);
+
+        int? maxDuplicate = analyzer.MaxDuplicateNumber();
+        if (maxDuplicate.HasValue)
+            Console.WriteLine("The maximum number that repeats more than once: " + maxDuplicate);
+        else
+            Console.WriteLine("No number repeats more than once.");
+    }
+}
